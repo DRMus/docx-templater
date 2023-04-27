6012ea45eb40ae16331d0343a0aed523c6dcb794
DRMus <78694252+DRMus@users.noreply.github.com>
2023-04-27 23:36:44 +0400

    Добавление electron, пересоздание проекта

--- Client/src/utils/loadFile.ts
+++
@@ -1,5 +0,0 @@
-import PizZipUtils from 'pizzip/utils/index.js';
-
-export default function loadFile(url: string, callback: (err: Error, data: string) => void) {
-  PizZipUtils.getBinaryContent(url, callback);
-}
